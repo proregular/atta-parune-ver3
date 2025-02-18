1306f299f7f611a1ef3f2e1f2e226121ea4bc63e
KwonHyeji <kwonhyeji007@gmail.com>
2025-02-18 17:27:48 +0900

    2025-02-18 JPA 설정

---
+++ src/main/java/com/green/attaparunever2/entity/BlackList.java
@@ -0,0 +1,12 @@
+package com.green.attaparunever2.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class BlackList extends CreatedAt {
+
+}
