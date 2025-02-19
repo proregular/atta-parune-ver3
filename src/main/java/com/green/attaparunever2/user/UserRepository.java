37ff652e2bff52bd923f766218f9b6d6bb9f0ea9
KwonHyeji <kwonhyeji007@gmail.com>
2025-02-19 17:33:18 +0900

    2025-02-19 수정

--- src/main/java/com/green/attaparunever2/user/UserRepository.java
+++ src/main/java/com/green/attaparunever2/user/UserRepository.java
@@ -2,7 +2,9 @@ package com.green.attaparunever2.user;
 
 import com.green.attaparunever2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
