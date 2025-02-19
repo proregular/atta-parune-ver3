2c8ce2471b202ebc2827c3c515c0b3a06bb3e11e
Lee eo jin <djwls7777@gmail.com>
2025-02-19 10:31:50 +0900

    jpa

---
+++ src/main/java/com/green/attaparunever2/entity/CompanyTicketCreationTime.java
@@ -0,0 +1,29 @@
+package com.green.attaparunever2.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+public class CompanyTicketCreationTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    private Long creationId;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @Column(nullable = false)
+    private LocalTime creationTimeStart;
+
+    @Column(nullable = false)
+    private LocalTime creationTimeEnd;
+
+    @Column(nullable = false, length = 30)
+    private String creationTimeName;
+}
---
+++ src/main/java/com/green/attaparunever2/entity/UserPointDeposit.java
@@ -0,0 +1,30 @@
+package com.green.attaparunever2.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.aspectj.apache.bcel.classfile.Code;
+
+@Entity
+@Getter
+@Setter
+public class UserPointDeposit extends UpdatedAt{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    private Long depositId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "admin_id", nullable = false)
+    private Admin admin;
+
+    @ManyToOne
+    @JoinColumn(name = "code", nullable = false)
+    private Code code;
+
+    @Column(nullable = false)
+    private int pointAmount;
+}
