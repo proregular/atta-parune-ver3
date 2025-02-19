eccf7986bd004cfcbc6580830509c6887a51e4d8
KimWooJun <wojun74@naver.com>
2025-02-19 13:17:18 +0900

    JPA 작업 찐찐막

--- src/main/java/com/green/attaparunever2/entity/SystemPost.java
+++ src/main/java/com/green/attaparunever2/entity/SystemPost.java
@@ -15,11 +15,11 @@ public class SystemPost extends UpdatedAt {
     private Long inquiryId;
 
     @ManyToOne
-    @JoinColumn(name = "admin_id", nullable = false)
+    @JoinColumn(name = "admin_id")
     private Admin admin;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne
