a531ba2f422dcc15b5813602613ec3850d3615a2
SugiSagong <jumoney1012@gmail.com>
2025-02-24 12:06:27 +0900

    2차 개선 병합작업 - 1

--- src/main/java/com/green/attaparunever2/restaurant/model/GetRestaurantMainReq.java
+++ src/main/java/com/green/attaparunever2/restaurant/model/GetRestaurantMainReq.java
@@ -1,8 +1,9 @@
 package com.green.attaparunever2.restaurant.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.green.attaparunever2.common.model.Paging;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,9 @@ import lombok.Setter;
 @Setter
 @Schema(title = "사용자 메인 화면 식당 리스트 요청")
 public class GetRestaurantMainReq extends Paging {
-    @Schema(title = "식당 카테고리 PK", example = "0", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "식당 카테고리 PK(1:한식, 2:중식, 3:일식)", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Max(value = 3, message = "존재하지 않는 기준 입니다.")
+    @Min(value = 1, message = "존재하지 않는 기준 입니다.")
     private long categoryId;
 
     public GetRestaurantMainReq(Integer page, Integer size, long categoryId) {
