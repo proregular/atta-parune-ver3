bd3baafdbe1e4d53f4465831e5a407517faaf6fd
Lee eo jin <djwls7777@gmail.com>
2025-02-24 16:05:19 +0900

    api 변경

--- src/main/java/com/green/attaparunever2/admin/AdminController.java
+++ src/main/java/com/green/attaparunever2/admin/AdminController.java
@@ -3,6 +3,8 @@ package com.green.attaparunever2.admin;
 import com.green.attaparunever2.admin.model.AdminSignUpReq;
 import com.green.attaparunever2.common.model.ResultResponse;
 import com.green.attaparunever2.admin.model.*;
+import com.green.attaparunever2.reservation.ReservationService;
+import com.green.attaparunever2.reservation.model.ReservationPostReq;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,6 +26,7 @@ import java.util.List;
 @Tag(name="관리자", description = "관리자 관련 API")
 public class AdminController {
     private final AdminService adminService;
+    private final ReservationService reservationService;
 
     @GetMapping
     @Operation(summary = "관리자 정보 조회")
@@ -90,7 +93,7 @@ public class AdminController {
                 .build();
     }
 
-    @PutMapping("/find-passowrd")
+    @PutMapping("/find-password")
     @Operation(summary = "비밀번호 찾기")
     public ResultResponse<Integer> findPassword(@Valid @RequestBody AdminFindPasswordReq p) {
         int result = adminService.findPassword(p);
@@ -101,19 +104,7 @@ public class AdminController {
                 .build();
     }
 
-    @GetMapping("v3/CompanyPayment")
-    @Operation(summary = "회사 포인트 판매 내역 조회")
-    public ResultResponse<List<getCompanyPaymentRes>> getCompanyPayment(){
-        List<getCompanyPaymentRes> res = adminService.getCompanyPayment();
-
-        return ResultResponse.<List<getCompanyPaymentRes>>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("회사 포인트 판매 내역조회")
-                .resultData(res)
-                .build();
-    }
-
-    @PostMapping("v3/signUpAdmin")
+    @PostMapping("v3/sign-up")
     @Operation(summary = "시스템 관리자 회원가입")
     public ResultResponse<Integer> signUpAdmin(@RequestBody SignAdminReq p) {
         int result = adminService.SignUpAdmin(p);
@@ -125,67 +116,7 @@ public class AdminController {
                 .build();
     }
 
-    @GetMapping("v3/Refund")
-    @Operation(summary = "환불 내역 조회")
-    public ResultResponse<List<SelRefundRes>> getRefund(){
-        List<SelRefundRes> res = adminService.getRefund();
-
-        return ResultResponse.<List<SelRefundRes>>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("환불 내역 조회 완료")
-                .resultData(res)
-                .build();
-    }
-
-    @GetMapping("v3/companyEnrollment")
-    @Operation(summary = "회사 제휴신청서 보기")
-    public ResultResponse<List<SelCompanyEnrollmentRes>> getCompanyEnrollment(){
-        List<SelCompanyEnrollmentRes> res = adminService.getCompanyEnrollment();
-
-        return ResultResponse.<List<SelCompanyEnrollmentRes>>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("회사 제휴신청서 보기 완료")
-                .resultData(res)
-                .build();
-    }
-
-    @GetMapping("v3/restaurantEnrollment")
-    @Operation(summary = "식당 입점신청서 보기")
-    public ResultResponse<List<SelRestaurantEnrollmentRes>> getRestaurantEnrollment(){
-        List<SelRestaurantEnrollmentRes> res = adminService.getRestaurantEnrollment();
-
-        return ResultResponse.<List<SelRestaurantEnrollmentRes>>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("식당 입점신청서 보기 완료")
-                .resultData(res)
-                .build();
-    }
-
-    @PostMapping("v3/restaurantEnrollment")
-    @Operation(summary = "식당 입점 신청서 작성")
-    public ResultResponse<Integer> postRestaurantEnrollment(@RequestBody InsRestaurantEnrollmentReq req){
-        int result = adminService.postRestaurantEnrollment(req);
-
-        return ResultResponse.<Integer>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("식당 입점 신청서 등록 완료")
-                .resultData(result)
-                .build();
-    }
-    
-    @PostMapping("v3/CompanyEnrollment")
-    @Operation(summary = "회사 제휴 신청서 등록")
-    public ResultResponse<Integer> postCompanyEnrollment(@RequestBody InsCompanyEnrollmentReq req){
-        int result = adminService.postCompanyEnrollment(req);
-
-        return ResultResponse.<Integer>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("회사 제휴 신청서 등록 완료")
-                .resultData(result)
-                .build();
-    }
-
-    @PutMapping("v3/admin")
+    @PutMapping("v3")
     @Operation(summary = "식당, 회사 회원가입")
     public ResultResponse<Integer> updAdmin(@RequestBody SignUpAdminReq req){
         int result = adminService.updAdmin(req);
@@ -197,43 +128,4 @@ public class AdminController {
                 .build();
     }
 
-    @PostMapping("v3/systemPost")
-    @Operation(summary = "게시글 등록하기")
-    public ResultResponse<Integer> postSystemPost(@RequestPart(required = false) MultipartFile pic
-                                                , @RequestPart InsSystemInquiryReq req){
-        int result = adminService.postSystemPost(pic, req);
-
-        return ResultResponse.<Integer>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("게시글 등록완료")
-                .resultData(result)
-                .build();
-    }
-
-
-    @GetMapping("v3/systemPost")
-    @Operation(summary = "게시글 자세히 보기")
-    public ResultResponse<SelOneSystemPostRes> getSelOneSystemPost(@ParameterObject @ModelAttribute long inquiryId){
-        SelOneSystemPostRes res = adminService.getOneSystemPost(inquiryId);
-
-        return ResultResponse.<SelOneSystemPostRes>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("게시글 자세히 보기 완료")
-                .resultData(res)
-                .build();
-
-    }
-
-    @PostMapping("v3/announcement")
-    @Operation(summary = "공지사항 등록하기")
-    public ResultResponse<Integer> postSystemPost(@RequestPart(required = false) MultipartFile pic
-                                                , @RequestPart InsAnnouncementReq req){
-        int result = adminService.postAnnouncement(pic, req);
-
-        return ResultResponse.<Integer>builder()
-                .statusCode(HttpStatus.OK.toString())
-                .resultMsg("공지사항 등록완료")
-                .resultData(result)
-                .build();
-    }
 }
--- src/main/java/com/green/attaparunever2/restaurant/RestaurantController.java
+++ src/main/java/com/green/attaparunever2/restaurant/RestaurantController.java
@@ -20,66 +20,6 @@ import java.util.List;
 public class RestaurantController {
     private final RestaurantService restaurantService;
 
-    @PostMapping("holiday")
-    @Operation(summary = "휴무일 등록")
-    public ResultResponse<Integer> postHoliday(@RequestBody InsHolidayReq p){
-        int result = restaurantService.postHoliday(p);
-
-        return ResultResponse.<Integer>builder()
-                .statusCode("200")
-                .resultMsg("휴무일 등록 성공")
-                .resultData(result)
-                .build();
-    }
-
-    @GetMapping("holiday")
-    @Operation(summary = "휴무일 보기")
-    public ResultResponse<List<SelHolidayRes>> getHoliday(@ParameterObject @ModelAttribute SelHolidayReq p){
-        List<SelHolidayRes> res = restaurantService.getHoliday(p);
-
-        return ResultResponse.<List<SelHolidayRes>>builder()
-                .statusCode("200")
-                .resultMsg("휴무일 보기 완료")
-                .resultData(res)
-                .build();
-    }
-
-    @PatchMapping
-    @Operation(summary = "식당 정보 수정")
-    public ResultResponse<Integer> updateRestaurant(@RequestBody UpdRestaurantReq req){
-        int result = restaurantService.patchRestaurant(req);
-
-        return ResultResponse.<Integer>builder()
-                .statusCode("200")
-                .resultMsg("식당 정보 수정 완료")
-                .resultData(result)
-                .build();
-    }
-
-    @PatchMapping("holiday")
-    @Operation(summary = "휴무일 변경")
-    public ResultResponse<Integer> updateHoliday(@RequestBody UpdHolidayReq req){
-        int result = restaurantService.patchHoliday(req);
-
-        return ResultResponse.<Integer>builder()
-                .statusCode("200")
-                .resultMsg("휴무일 변경 완료")
-                .resultData(result)
-                .build();
-    }
-
-    @GetMapping("orderList")
-    @Operation(summary = "식당 매출정보 리스트 조회")
-    public ResultResponse<List<SelRestaurantOrderRes>> getRestaurantPoint(@ParameterObject @ModelAttribute SelRestaurantOrderReq p){
-        List<SelRestaurantOrderRes> res = restaurantService.getRestaurantPoint(p);
-
-        return ResultResponse.<List<SelRestaurantOrderRes>>builder()
-                .statusCode("200")
-                .resultMsg("식당 매출 정보 대시보드 정보 조회 완료.")
-                .resultData(res)
-                .build();
-    }
-
     //------------------------------------------------------------------------------------------------------------------------------------------
 
     @GetMapping("v3/main")
