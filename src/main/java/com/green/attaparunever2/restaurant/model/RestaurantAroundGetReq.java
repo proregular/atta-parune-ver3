45b5984003601e546119d7403356b71ae7cc8236
KwonHyeji <kwonhyeji007@gmail.com>
2025-02-27 17:47:29 +0900

    식당 찾기 탭 식당 리스트 조회 수정 완료

--- src/main/java/com/green/attaparunever2/restaurant/model/RestaurantAroundGetReq.java
+++ src/main/java/com/green/attaparunever2/restaurant/model/RestaurantAroundGetReq.java
@@ -15,7 +15,7 @@ public class RestaurantAroundGetReq {
     @Schema(description = "검색 단어", example = "돈까스")
     private String searchFilter;
     @Schema(description = "정렬 기준(0:전체, 1:거리순, 2:식사순 3:별점순)", example = "0")
-    @Max(value = 1, message = "존재하지 않는 기준 입니다.")
+    @Max(value = 3, message = "존재하지 않는 기준 입니다.")
     @Min(value = 0, message = "존재하지 않는 기준 입니다.")
     private int orderFilter;
     @Schema(description = "사용자의 위도", example = "35.86")
