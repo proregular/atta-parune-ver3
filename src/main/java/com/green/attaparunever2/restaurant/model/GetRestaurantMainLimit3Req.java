476f8648164cbdb851071e90cdcfb95b3cdfbfd8
Lee eo jin <djwls7777@gmail.com>
2025-02-27 15:40:21 +0900

    메인 화면 추천 식당 조회 작업 완료

---
+++ src/main/java/com/green/attaparunever2/restaurant/model/GetRestaurantMainLimit3Req.java
@@ -0,0 +1,12 @@
+package com.green.attaparunever2.restaurant.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetRestaurantMainLimit3Req {
+    @JsonIgnore
+    private int filterType;
+}
