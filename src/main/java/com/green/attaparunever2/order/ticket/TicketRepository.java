64134f1f3c4e9d3f9242b70e1fa60e73ebac5e55
Lee eo jin <djwls7777@gmail.com>
2025-02-28 20:13:16 +0900

    식권 결제 작업 개선 완료

--- src/main/java/com/green/attaparunever2/order/ticket/TicketRepository.java
+++ src/main/java/com/green/attaparunever2/order/ticket/TicketRepository.java
@@ -3,11 +3,20 @@ package com.green.attaparunever2.order.ticket;
 import com.green.attaparunever2.entity.Order;
 import com.green.attaparunever2.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Optional<Ticket> findByOrder(Order order);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Ticket t SET t.ticketStatus = :status, t.useDate = :useDate WHERE t.ticketId = :ticketId")
+    int updateTicketStatusAndUseDate(Long ticketId, int status, LocalDateTime useDate);
 }
