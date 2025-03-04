c8f64a69ae58241f4d690e188ab08d566fdc3da0
SugiSagong <jumoney1012@gmail.com>
2025-03-04 17:36:48 +0900

    feat: ㅁㅁ

--- src/main/java/com/green/attaparunever2/user/model/SelUserOrderPastCheckReq.java
+++ src/main/java/com/green/attaparunever2/user/model/SelUserOrderPastCheckReq.java
@@ -7,6 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SelUserOrderPastCheckReq {
-    @Schema(description = "주문 PK", example = "1")
-    private long orderId;
+    /*@Schema(description = "주문 PK", example = "1")
+    private long orderId;*/
 }
