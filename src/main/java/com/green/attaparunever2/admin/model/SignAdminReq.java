394547f5c910d22b5d9bfa306d93825e44c24356
SugiSagong <jumoney1012@gmail.com>
2025-03-07 10:44:50 +0900

    feat: 요청 사항 수정

--- src/main/java/com/green/attaparunever2/admin/model/SignAdminReq.java
+++ src/main/java/com/green/attaparunever2/admin/model/SignAdminReq.java
@@ -1,6 +1,7 @@
 package com.green.attaparunever2.admin.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +10,11 @@ import lombok.Setter;
 @Setter
 public class SignAdminReq {
     @Schema(description = "아이디", example = "qwe")
+    @JsonProperty("id")
     private String aid;
 
     @Schema(description = "비밀번호", example = "qwe")
+    @JsonProperty("pw")
     private String apw;
 
     @Schema(description = "이름", example = "홍길동")
