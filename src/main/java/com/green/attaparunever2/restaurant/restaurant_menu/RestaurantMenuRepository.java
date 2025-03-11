2cb6063e299f780191ad7184bb125d39602897be
Lee eo jin <djwls7777@gmail.com>
2025-03-11 10:13:50 +0900

    메뉴 카테고리 삭제 수정

--- src/main/java/com/green/attaparunever2/restaurant/restaurant_menu/RestaurantMenuRepository.java
+++ src/main/java/com/green/attaparunever2/restaurant/restaurant_menu/RestaurantMenuRepository.java
@@ -1,10 +1,11 @@
 package com.green.attaparunever2.restaurant.restaurant_menu;
 
 import com.green.attaparunever2.entity.RestaurantMenu;
+import com.green.attaparunever2.entity.RestaurantMenuCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantMenuRepository extends JpaRepository<RestaurantMenu, Long> {
-
+    int countByCategoryId(RestaurantMenuCategory category);
 }
